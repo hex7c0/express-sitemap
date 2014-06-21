1e96ec15ca2743cebc35aa70f8e045b47d670106
hex7c0 <carniellifrancesco@gmail.com>
2014-06-21 07:17:46 +0200

    middleware temporary fix * robots.txt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @module express-sitemap
  * @package express-sitemap
  * @subpackage main
- * @version 1.0.1
+ * @version 1.1.0
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
@@ -25,20 +25,146 @@ try {
  * functions
  */
 /**
+ * write data to file
+ * 
+ * @function write
+ * @param {String} data - created xml or robots.txt
+ * @param {String} file - name of file
+ * @return {Boolean}
+ */
+function write(data,file) {
+
+    return FS.writeFile(file,data,function(err) {
+
+        if (err) {
+            return false;
+        }
+        return true;
+    });
+}
+/**
+ * stream data to web
+ * 
+ * @function stream
+ * @param {String} data - created xml or robots.txt
+ * @param {Object} res - response to client
+ * @return
+ */
+function stream(data,res) {
+
+    res.send(data);
+    return;
+}
+
+/*
+ * class
+ */
+/**
+ * export class
+ * 
+ * @exports sitemap
+ */
+module.exports = function(options) {
+
+    return new sitemap(options);
+};
+/**
+ * sitemap class
+ * 
+ * @class sitemap
+ * @param {Object} options - various options. Check README.md
+ * @return {Object}
+ */
+function sitemap(options) {
+
+    options = options || {};
+    var http = options.http == 'https' ? 'https://' : 'http://';
+    var url = String(options.url || '127.0.0.1');
+    var port = Number(options.port) ? ':' + Number(options.port) : '';
+    this.my = {
+        file: String(options.file || 'sitemap.xml'),
+        url: http + url + port,
+        robots: Boolean(options.robots),
+        route: typeof (options.route) == 'object' ? options.route : Object.create(null),
+    };
+    this.sitemap = typeof (options.sitemap) == 'object' ? options.sitemap : Object.create(null);
+    if (options.generate && options.generate._router) {
+        this.generate(options.generate);
+    }
+    return;
+}
+/**
+ * generate sitemap object
+ * 
+ * @function generate
+ * @param {Object} app - express app
+ * @return {Object}
+ */
+sitemap.prototype.generate = function(app) {
+
+    var that = this.sitemap;
+    var routing = app._router.stack;
+    for (var i = 0, il = routing.length; i < il; i++) {
+        var route = routing[i].route;
+        if (route) {
+            var path = route.path;
+            if (that[path] == undefined) {
+                that[path] = [];
+            }
+            for ( var name in route.methods) {
+                if (that[path].indexOf(name) < 0) {
+                    that[path].push(name);
+                }
+            }
+        }
+    }
+    return that;
+};
+/**
+ * generate sitemap object with tickle
+ * 
+ * @function tickle
+ * @return {Object}
+ */
+sitemap.prototype.tickle = function() {
+
+    if (GLOBAL.tickle && GLOBAL.tickle.route) {
+        var that = this.sitemap;
+        var routing = GLOBAL.tickle.route;
+        for ( var route in routing) {
+            that[route] = [];
+        }
+        return that;
+    }
+    return Object.create(null);
+};
+/**
+ * reset
+ * 
+ * @function reset
+ * @return
+ */
+sitemap.prototype.reset = function() {
+
+    this.sitemap = Object.create(null);
+    return;
+};
+/**
  * create xml from sitemap
  * 
  * @function xml
- * @return {Object}
+ * @return {String}
  */
 sitemap.prototype.xml = function() {
 
     var temp = null;
     var route = this.my.route;
+    var sitemap = this.sitemap;
     var data = '<?xml version="1.0" encoding="UTF-8"?>';
     data += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-    for ( var uri in this.sitemap) {
+    for ( var uri in sitemap) {
         var rr = route[uri] || route['ALL'] || false;
-        if (!route[uri].disallow) {
+        if (!rr || !rr.disallow) {
             data += '<url>';
             // loc
             data += '<loc>';
@@ -69,126 +195,85 @@ sitemap.prototype.xml = function() {
     return data;
 };
 /**
- * write sitemap to file
+ * create robots.txt from sitemap
  * 
- * @function write
- * @param {String} data - created xml
- * @return {Boolean}
+ * @function robots
+ * @return {String}
  */
-sitemap.prototype.write = function(data) {
+sitemap.prototype.robots = function() {
 
-    return FS.writeFile(this.my.file,data,function(err) {
-
-        if (err) {
-            return false;
+    var temp = 0;
+    var route = this.my.route;
+    var sitemap = this.sitemap;
+    var data = 'User-agent: *\n';
+    for ( var uri in sitemap) {
+        if (route['ALL'] && route['ALL'].disallow) {
+            temp++;
+            data += 'Disallow: /\n';
+            break;
+        } else if (route[uri] && route[uri].disallow) {
+            temp++;
+            data += 'Disallow: ';
+            data += uri;
+            data += '\n';
         }
-        return true;
-    });
+    }
+    if (temp == 0) {
+        data += 'Disallow: \n';
+    }
+    return data;
 };
 /**
- * alis for write sitemap to file
+ * alias for write sitemap to file
+ * 
+ * @function XMLtoFile
+ * @return {Boolean}
+ */
+sitemap.prototype.XMLtoFile = function() {
+
+    return write(this.xml(),this.my.file);
+};
+/**
+ * alias for write robots.txt to file
+ * 
+ * @function TXTtoFile
+ * @return {Boolean}
+ */
+sitemap.prototype.TXTtoFile = function() {
+
+    return write(this.robots(),'robots.txt');
+};
+/**
+ * alias for write both to files
  * 
  * @function toFile
  * @return {Boolean}
  */
 sitemap.prototype.toFile = function() {
 
-    return this.write(this.xml());
+    return write(this.xml(),this.my.file) && write(this.robots(),'robots.txt');
 };
 /**
- * stream sitemap to web
+ * alias for stream sitemap to web
  * 
- * @function web
- * @param {String} data - created xml
+ * @function XMLtoWeb
  * @param {Object} res - response to client
  * @return
  */
-sitemap.prototype.stream = function(data,res) {
+sitemap.prototype.XMLtoWeb = function(res) {
 
     res.header('Content-Type','application/xml');
-    res.send(data);
-    return;
+    return stream(this.xml(),res);
 };
 /**
- * alis for stream sitemap to web
+ * alias for stream robots.txt to web
  * 
- * @function toFile
+ * @function TXTtoWeb
  * @param {Object} res - response to client
  * @return
  */
-sitemap.prototype.toWeb = function(res) {
+sitemap.prototype.TXTtoWeb = function(res) {
 
-    return this.stream(this.xml(),res);
-};
-/**
- * generate sitemap
- * 
- * @function write
- * @param {Object} app - express app
- * @return {Object}
- */
-sitemap.prototype.generate = function(app) {
-
-    var that = this.sitemap;
-    var routing = app._router.stack;
-    for (var i = 0, il = routing.length; i < il; i++) {
-        var route = routing[i].route;
-        if (route) {
-            var path = route.path;
-            if (that[path] == undefined) {
-                that[path] = [];
-            }
-            for ( var name in route.methods) {
-                if (that[path].indexOf(name) < 0) {
-                    that[path].push(name);
-                }
-            }
-        }
-    }
-    return that;
-};
-/**
- * reset
- * 
- * @function reset
- * @return
- */
-sitemap.prototype.reset = function() {
-
-    this.sitemap = Object.create(null);
-    return;
-};
-/**
- * sitemap class
- * 
- * @class sitemap
- * @param {Object} options - various options. Check README.md
- * @return {Object}
- */
-function sitemap(options) {
-
-    var options = options || {};
-    var http = options.http == 'https' ? 'https://' : 'http://';
-    var url = String(options.url || '127.0.0.1');
-    var port = Number(options.port) ? ':' + Number(options.port) : '';
-    this.my = {
-        file: String(options.file || 'sitemap.xml'),
-        url: http + url + port,
-        robots: Boolean(options.robots),
-        route: typeof (options.route) == 'object' ? options.route : Object.create(null),
-    };
-    this.sitemap = typeof (options.sitemap) == 'object' ? options.sitemap : Object.create(null);
-    if (options.generate && options.generate._router) {
-        this.generate(options.generate);
-    }
-    return;
-}
-/**
- * export class
- * 
- * @exports sitemap
- */
-module.exports = function(options) {
-
-    return new sitemap(options);
+    res.header('Content-Type','text/plain');
+    return stream(this.robots(),res);
 };
