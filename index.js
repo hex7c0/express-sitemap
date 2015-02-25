f23f269c46c0b412798afa6f52b1560909b41e09
hex7c0 <carniellifrancesco@gmail.com>
2015-02-25 10:08:25 +0100

    build: use setheaders

--- index.js
+++ index.js
@@ -14,6 +14,7 @@
  */
 // import
 var fs = require('fs');
+var setHeaders = require('setheaders').setWritableHeader;
 
 /*
  * functions
@@ -43,7 +44,8 @@ function write(data, file) {
  */
 function stream(data, res, header) {
 
-  return res.header('Content-Type', header).send(data);
+  return setHeaders(res, 'Content-Type', header) === true ? res.send(data)
+    : null;
 }
 
 /**
